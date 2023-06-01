2c488e961ee8ba9547e2153740edb5b5a5dc764c
Divesh Agarwal <diveshagarwal1995@gmail.com>
2023-06-01 17:37:30 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const App = () => {
+    return ( <
+        >
+        <
+        div className = "main_div" >
+        <
+        div className = "center_div" >
+        <
+        h1 > ToDo List < /h1>  <
+        br / >
+        <
+        input type = "text"
+        placeholder = "Add Item" / >
+        <
+        button > + < /button>
+
+        <
+        /div >  <
+        /div >  <
+        / >
+    );
+};
+export default App;
