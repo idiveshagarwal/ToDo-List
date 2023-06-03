8be25e2ed0aacf0614a1d05ad3d03007f4bb75bd
Divesh Agarwal <diveshagarwal1995@gmail.com>
2023-06-03 11:59:48 +0530

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+RecatDOM.render(
+    <App />,
+    document.getElementById('root')
+);
